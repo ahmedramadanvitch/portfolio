1a013cd908db08cc06d00d23ce619c9a97001d9f
ahmedramadanvitch <ahmedramadanvitch989.gmail.com>
2024-05-16 11:13:46 +0300

    update my cv

--- src/app/(pages)/cv/page.jsx
+++ src/app/(pages)/cv/page.jsx
@@ -6,7 +6,7 @@ export default function Cv() {
       <div className="w-full h-[50vh] flex justify-center items-center">
         <a
           download="ahmed-ramadan-cv"
-          href="./Ahmed Ramadan CV.pdf"
+          href="./ahmed ramadan cv.pdf"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full tracking-wider"
         >
           Download CV
